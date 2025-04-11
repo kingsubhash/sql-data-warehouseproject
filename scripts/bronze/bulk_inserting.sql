cde2b74465b630a76ef4be4a5801031d54aa51e2
Subhash Khanal <33798983+kingsubhash@users.noreply.github.com>
2025-04-10 23:13:53 -0500

    Update bulk_inserting.sql

--- scripts/bronze/bulk_inserting.sql
+++ scripts/bronze/bulk_inserting.sql
@@ -1,68 +1,149 @@
+
 /*
-This is just basic bulk inserting , where we are inserting data from our source file
-First we are truncating , which means deleting the table and then inserting the data into their respective table.
-But this is not the recommened method whie inserting, since we need to create a procedure , and by just one line of code we can load all the data 
-Procedure method is explained on the "procedure_insert.sql"
+
+If we have scripts of sql that is used frequently, then we can create stored procedure
+
+Stored Procedure can be found under Progammability and can be exected by just calling it with exec function, 
+
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
 */
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '================================================';
+
+		PRINT '------------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '------------------------------------------------';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_cust_info';
+		TRUNCATE TABLE bronze.crm_cust_info;
+		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+		BULK INSERT bronze.crm_cust_info
+		FROM 'E:\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_prd_info';
+		TRUNCATE TABLE bronze.crm_prd_info;
+
+		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
+		BULK INSERT bronze.crm_prd_info
+		FROM 'E:\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_sales_details';
+		TRUNCATE TABLE bronze.crm_sales_details;
+		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
+		BULK INSERT bronze.crm_sales_details
+		FROM 'E:\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		PRINT '------------------------------------------------';
+		PRINT 'Loading ERP Tables';
+		PRINT '------------------------------------------------';
+		
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_loc_a101';
+		TRUNCATE TABLE bronze.erp_loc_a101;
+		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'E:\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12;
+		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'E:\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'E:\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		SET @batch_end_time = GETDATE();
+		PRINT '=========================================='
+		PRINT 'Loading Bronze Layer is Completed';
+        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '=========================================='
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+END
 
 
-
-TRUNCATE TABLE bronze.crm_cust_info
-BULK INSERT bronze.crm_cust_info
-FROM 'E:\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH(
-	FIRSTROW=2,-- First row is always header
-	FIELDTERMINATOR = ',',--Field seperator
-	TABLOCK--Locking entire table for performance issue 
-);
-/* Better to check the quality of insert after executing the insert query
-Select * from bronze.crm_cust_info;
-*/
-TRUNCATE TABLE bronze.crm_prd_info
-BULK INSERT bronze.crm_prd_info
-FROM 'E:\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH(
-	FIRSTROW=2,-- First row is always header
-	FIELDTERMINATOR = ',',--Field seperator
-	TABLOCK--Locking entire table for performance issue 
-);
-
-/* Better to check the quality of insert after executing the insert query
-Select * from bronze.crm_prd_info;
-*/
-TRUNCATE TABLE bronze.crm_sales_details
-BULK INSERT bronze.crm_sales_details
-FROM 'E:\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH(
-	FIRSTROW=2,-- First row is always header
-	FIELDTERMINATOR = ',',--Field seperator
-	TABLOCK--Locking entire table for performance issue 
-);
-
---Select *from bronze.crm_sales_details
-TRUNCATE TABLE bronze.erp_cust_az12
-BULK INSERT bronze.erp_cust_az12
-FROM 'E:\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-WITH(
-	FIRSTROW=2,-- First row is always header
-	FIELDTERMINATOR = ',',--Field seperator
-	TABLOCK--Locking entire table for performance issue 
-);
-
---Select * from bronze.erp_cust_az12
-TRUNCATE TABLE bronze.erp_loc_a101
-BULK INSERT bronze.erp_loc_a101
-FROM 'E:\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-WITH(
-	FIRSTROW=2,-- First row is always header
-	FIELDTERMINATOR = ',',--Field seperator
-	TABLOCK--Locking entire table for performance issue 
-);
-TRUNCATE TABLE bronze.erp_px_cat_g1v2
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'E:\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-WITH(
-	FIRSTROW=2,-- First row is always header
-	FIELDTERMINATOR = ',',--Field seperator
-	TABLOCK--Locking entire table for performance issue 
-);
